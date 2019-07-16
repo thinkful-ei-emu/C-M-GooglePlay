35b32b54266eb674cecd44c56caaa792c0637c78
Martha Plantz <maplantz89@gmail.com>
2019-07-16 09:11:40 -0700

    finished working on the playstore assignment

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const playstore =  require('./playstore');
+
+const app=express();
+app.use(morgan('dev'));
+
+app.get('/apps', (req, res) => {
+  let filteredApps = [...playstore];
+  const {sort, genres} = req.query;
+
+  const validSorts = ['Rating', 'App'];
+  if(sort && !validSorts.includes(sort)){
+    return res.status(400).json({error: 'Sort must be either Rating or App.'});
+  }
+
+  if(sort==='Rating'){
+    filteredApps.sort((a, b) => a[sort] > b[sort] ? -1 : 1);
+  }
+
+  if(sort === 'App'){
+    filteredApps.sort((a, b) => a[sort] > b[sort] ? 1 : -1);
+  }
+
+  const validGenres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
+  if(genres && !validGenres.includes(genres)){
+    return res.status(400).json({error: 'Genres must be one of the following: Action, Puzzle, Strategy, Casual, Arcade, Card'});
+  }
+
+  if (genres){
+    filteredApps=filteredApps.filter(app => app.Genres.toLowerCase().includes(genres.toLowerCase()));
+  }  
+
+  res.json(filteredApps);
+});
+
+
+app.listen(8000, ()=> console.log('Server running on Port 8000'));
